616e6f7be28f627e9d668ba7a665ab791de7d9c9
Francielly Pazello <74922124+franpazello96@users.noreply.github.com>
2025-03-16 16:05:41 -0300

    feat: signup

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -5,8 +5,9 @@ import { ThemeToggle } from "@/components/ThemeToggle";
 import Image from "next/image";
 import logo from "@/assets/logo.png";
 import { useState } from "react";
+import { Button } from "@/components/button";
 
-export default function signin(){
+export default function Signin(){
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -25,29 +26,28 @@ export default function signin(){
         <Image src={logo} alt="Logo" width={108.5} height={30} />
         <ThemeToggle />
       </div>
+
+
       <h1 className="flex text-4xl leading-none font-medium md:text-4xl mt-10
       text-zinc-800 dark:text-zinc-100 justify-center items-center">
       Login </h1>
 
       <form action="" onSubmit={handleSubmit}>
-      <Input 
-        type="email" 
-        placeholder="E-mail" 
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <Input 
-        type="password" 
-        placeholder="Senha" 
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
+        <Input 
+          type="email" 
+          placeholder="E-mail" 
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input 
+          type="password" 
+          placeholder="Senha" 
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
 
-      <button type="submit" className="bg-zinc-500 dark:bg-zinc-700 text-zinc-100 dark:text-zinc-100
-      rounded-lg p-2 w-full mt-4 hover:cursor-pointer hover:bg-zinc-400 dark:hover:bg-zinc-600">Entrar</button>
+        <Button type="submit"> Entrar </Button>
       </form>
-
-
     </div>
   )
 }
