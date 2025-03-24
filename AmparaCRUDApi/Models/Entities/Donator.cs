709abf98a83f8f8614b3113a0780105660de5952
Fkureski <felipekureski@gmail.com>
2025-03-24 14:12:47 -0300

    CRUD API started / Entity Framework instaled / Connection with db made standardized

---
+++ AmparaCRUDApi/Models/Entities/Donator.cs
@@ -0,0 +1,11 @@
+﻿namespace AmparaCRUDApi.Models.Entities
+{
+    public class Donator
+    {
+        public required string CPF { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Password { get; set; }
+    }
+}
