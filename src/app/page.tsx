71c0f80dac37cd633ca4b94f9119f4ab87ff2663
Francielly Pazello <74922124+franpazello96@users.noreply.github.com>
2025-03-31 20:02:46 -0300

    feat:readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import logo from "../assets/logo.png";
 import Image from 'next/image'
+import Footer from "@/components/Footer/page";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center">
       <Image src={logo} alt="Logo" width={108.5} height={30} />
       <ThemeToggle />
+      <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer/page.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="mt-5">
+          <p className="text-gray-600 dark:text-gray-400">
+            © 2025 Ampara. Todos os direitos reservados. 
+          </p>
+    </footer>
+  );
+}
