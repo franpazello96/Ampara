2e0541e81d1228b4cd91475de13a59bb4999fe7c
Fkureski <felipekureski@gmail.com>
2025-04-07 15:01:47 -0300

    Donee API signup

---
+++ AmparaCRUDApi/Migrations/20250407175931___EFMigrationsHistory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmparaCRUDApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class __EFMigrationsHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Donees",
+                columns: table => new
+                {
+                    CNPJ = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    InstitutionName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InstitutionType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RepresentativeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donees", x => x.CNPJ);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Donees");
+        }
+    }
+}
---
+++ AmparaCRUDApi/Models/AddDoneeDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmparaCRUDApi.Models
+{
+    public class AddDoneeDTO
+    {
+        [Required]
+        public string CNPJ {  get; set; }
+        [Required]
+        public string InstitutionName { get; set; }
+        [Required]
+        public string InstitutionType { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string RepresentativeName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
