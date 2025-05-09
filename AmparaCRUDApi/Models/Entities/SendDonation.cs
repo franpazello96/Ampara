728a04f95eaa1b96363ace760b23c3d2f8523df8
Your Name <felipepoulos@gmail.com>
2025-05-09 17:24:43 -0300

    Started Buys and SendDonation

---
+++ AmparaCRUDApi/Models/Entities/SendDonation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AmparaCRUDApi.Models.Entities
+{
+    public class SendDonation
+    {
+        [Key]
+        public int ID { get; set; }
+        public decimal amount { get; set; }
+
+    }
+}
